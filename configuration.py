f76195b849dd9b1872466c452878b4b7fd3f9ee2
Valentina <val97392824@yandex.ru>
2024-08-27 16:15:52 +0700

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+
+URL_SERVICE = "https://8dae76c8-ea4d-4262-9c2b-8bc783a61366.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,46 @@
+
+import requests
+import configuration
+import data
+
+
+import requests
+
+import configuration
+
+
+
+def get_users_table():
+
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+
+
+response = get_users_table()
+
+print(response.status_code)
+
+
+def post_new_user(body):
+
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+
+response = post_new_user(data.user_body)
+
+print(response.status_code)
+
+print(response.json())
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,
+                         headers=data.headers)
+response = post_products_kits(data.product_ids)
+
+print(response.status_code)
+print(response.json())
+
+
